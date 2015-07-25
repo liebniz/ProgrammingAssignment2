e35012b60d9a4c7ba98e10bd20a6048ad0c7b53b
liebniz <jzendle@hotmail.com>
2015-07-24 22:37:40 -0400

    Update cachematrix.R
    add comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,23 +1,35 @@
-## Put comments here that give an overall description of what your
+## The following functions caclulate the inverse of a matrix and cache the results using R scoping rules 
+## The intent is to perform the expensive invers operation once per matrix inverted
 ## functions do
 
-## Write a short comment describing this function
+## Example 
+## A <- matrix( c(2, 4, 3, 1, 5, 7), nrow=2,ncol=3, byrow = TRUE)
+## cachedMatrix <- makeCacheMatrix(A)
+
+## INV <- cacheSolve(cachedMatrix) # first call
+
+## INV <-cacheSolve(cachedMatrix) # second call returns cached result from previous call
+## "getting cached data"
+
+
+## Create a function that calulates the inverse of a matrix
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
   set <- function(y) {
-    x <<- y
+    x <<- y  ## x must exist in the global (calling) env
     inv <<- NULL
   }
   get <- function() x
-  setinverse <- function(inverse) inv <<- inverse
-  getinverse <- function() inv
+  setinverse <- function(inverse) inv <<- inverse  ## this calulates the inverse
+  getinverse <- function() inv   ## return the inverse
   list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+## Solve for the inverse but only the first time called
+## Subsequent calls return the cached result
 cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x'
+  ## Return a the inverse matrix
   inv <- x$getinverse()
   if(!is.null(inv)) {
     message("getting cached data.")
